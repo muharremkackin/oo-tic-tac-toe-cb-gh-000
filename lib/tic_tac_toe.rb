d6a20113bcfd85d17e32fc5070a1e015356237d0
Muharrem Kaçkın <muharremkackin@gmail.com>
2018-05-16 00:08:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,8 +61,8 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.each do |combo|
-      board[combo[0]] == board[combo[1]] &&
-      board[combo[0]] == board[combo[2]] &&
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[0]] == @board[combo[2]] &&
       position_taken?(combo[0])
     end
   end
